15bfb410acb0b0100eff8ec8e1332df116e4852c
Dmitry Buevich <dmitry.buevich@mapbox.com>
2023-04-16 22:33:39 +0300

    Lection 26: Django admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    readonly_fields = ['votes']
+    extra = 1
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    readonly_fields = ['was_published_recently']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date', 'was_published_recently']})
+    ]
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    inlines = [ChoiceInline]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text', 'get_question_text', 'votes']
+    readonly_fields = ['votes']
+    search_fields = ['choice_text', 'question__question_text']
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -6,6 +7,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    @admin.display(
+        boolean=True,
+        description='Published recently?',
+        ordering='pub_date'
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - timezone.timedelta(days=1) <= self.pub_date <= now
@@ -21,5 +27,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return f'{self.question.question_text} : {self.choice_text}'
+
+    @admin.display(ordering='question__question_text', description='Question Text')
+    def get_question_text(self):
+        return self.question.question_text
 
 
