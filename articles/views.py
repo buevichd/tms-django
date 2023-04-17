a0d612dee8251e097d1a249f0e68297e28800656
Dmitry Buevich <dmitry.buevich@mapbox.com>
2023-04-17 23:32:19 +0300

    Homework 23-24

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article
 
-# Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+    context = {'all_articles': articles}
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, article_id: int):
+    article = get_object_or_404(Article, id=article_id)
+    context = {'article': article}
+    return render(request, 'articles/detail.html', context)
+
+
+def like(request):
+    assert request.method == 'POST'
+    article_id = request.POST['article_id']
+    article = get_object_or_404(Article, id=article_id)
+    article.like_count += 1
+    article.save()
+    return redirect('articles:detail', article.id)
